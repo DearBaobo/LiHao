81df5d7366a0d76574a4576a466ec5f491ade45a
李豪 <1633874215@qq.com>
2019-05-12 21:46:44 +0800

    提交

---
+++ src/Servlet/A.java
@@ -0,0 +1,105 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.corba.se.pept.transport.Connection;
+
+public class A extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public A() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Connection con = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con=(Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/studb","root","0920");
+				System.out.println("成功");
+				
+				Statement sta= (Statement)((java.sql.Connection) con).createStatement();
+				String user = request.getParameter("user");
+				String pwd = request.getParameter("pwd");
+				int num=sta.executeUpdate("insert dl values ('"+user+"','"+pwd+"') ");
+				if(num>0){
+					System.out.print("添加成功");
+				}
+			 }
+			 catch(ClassNotFoundException e){
+				 e.printStackTrace();
+				 System.out.print("没找到");
+			 }
+			 catch(SQLException sqle){
+				 System.out.print("连接异常");
+			 }
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
